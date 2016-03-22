404ada2f7fd62f819bd76d451e75ab921830bb53
Heshy Roskes <jroskes1@jhu.edu>
2016-03-22 15:56:23 -0400

    run analyzer script

---
+++ phantom_templates/runanalyzer.py
@@ -0,0 +1,48 @@
+import contextlib
+import os
+
+@contextlib.contextmanager
+def cd(path):
+   """http://stackoverflow.com/questions/24469538/sh-cd-using-context-manager"""
+   old_path = os.getcwd()
+   os.chdir(path)
+   try:
+       yield
+   finally:
+       os.chdir(old_path)
+
+template = """
+#!/bin/bash
+#SBATCH --job-name=JOBNAME
+#SBATCH --time=48:0:0
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=shared
+#SBATCH --mem=5000
+#SBATCH --mail-type=end
+#SBATCH --mail-user=heshyslurm@gmail.com
+
+cd {CMSSW_BASE}
+eval $(scram ru -sh)
+cd $SLURM_SUBMIT_DIR
+analyzer indir=$(pwd) phamom.lhe outdir=$(pwd) outfile=phamom.root computeVBFProdAngles=1 computeVHProdAngles=3
+""".strip()
+
+def runanalyzer(folder):
+    with cd(folder):
+        command = ""
+        if not os.path.exists("phamom.root"):
+            if not os.path.exists("phamom.lhe"):
+                command += "cat */phamom.dat > phamom.lhe\n"
+            themap = {
+                      "CMSSW_BASE": os.environ["CMSSW_BASE"],
+                     }
+            command += template.format(**themap)
+            with open("slurm.sh", "w") as f:
+                f.write(command)
+            os.system("sbatch slurm.sh")
+
+if __name__ == "__main__":
+    for a in os.listdir("."):
+        if a.startswith("gen_") and os.path.isdir(a):
+            runanalyzer(a)
